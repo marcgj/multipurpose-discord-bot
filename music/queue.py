1d8d83b5068890a2ce390590fc6ddf1d4279e344
marcgj <mgaspjoval2002@gmail.com>
2023-04-12 19:15:32 +0200

    added

---
+++ music/queue.py
@@ -0,0 +1,29 @@
+from music.track import ITrack
+
+
+class Queue():
+    queue = []
+    
+    def add_track(self, track: ITrack):
+        """
+        Ads a track to the queue
+
+        Args:
+            track (ITrack): Track to be added
+        """
+        self.queue.append(track)
+        
+        
+    def pop_track(self) -> ITrack:
+        """Returns and removes the first track of the queue"""
+        return self.queue.pop(0)
+        
+    def get_next() -> ITrack:
+        """Returns the first track of the queue without deleting it"""
+        return Queue[0]
+    
+    def is_empty(self):
+        """Returns whether the queue is empty or not
+        """
+        return True if len(self.queue) == 0 else False
+    
