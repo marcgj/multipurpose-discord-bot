8c8b701351ad1818c5ad8a74605ac5c502fc129d
marcgj <mgaspjoval2002@gmail.com>
2023-04-12 19:14:47 +0200

    added join command and changed play to new system

--- commands/music.py
+++ commands/music.py
@@ -2,13 +2,35 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-from music.sources import YoutubeSource
-from discord import FFmpegOpusAudio
+from music.track import YoutubeTrack
+from music.manager import GuildMusicManager
 
 
 class Music(commands.Cog):
+    music_manager = None
+
     def __init__(self, bot):
         self.bot = bot
+
+    @app_commands.command(name="join", description="Joins the user voice channel")
+    async def join(self, interaction: discord.Interaction):
+        await self._join(interaction)
+
+    async def _join(self, interaction: discord.Interaction):
+        # Get the voice client from the guild
+        voice = interaction.guild.voice_client
+
+        # TODO: Check if whoever uses the command is in a channel
+        # Get the voice channel where the user is connected
+        channel = interaction.user.voice.channel
+
+        # If bot is not already connected to a channel connect it
+        if not voice:
+            voice = await channel.connect()
+
+        # Generate a music manager instance
+        # !Intended for single guild use
+        self.music_manager = GuildMusicManager(voice)
 
     @app_commands.describe(query="Name or url of a song")
     @app_commands.command(
@@ -18,23 +40,18 @@ class Music(commands.Cog):
     async def play(self, interaction: discord.Interaction, query: str):
         await interaction.response.send_message(f"Searching for {query}")
 
-        # TODO: Check if whoever uses the command is in a channel
-        # Get the voice channel where the user is connected
-        channel = interaction.user.voice.channel
-
         # Get the voice client from the guild
         voice = interaction.guild.voice_client
 
         # If bot is not already connected to a channel connect it
         if not voice:
-            voice = await channel.connect()
+            await self._join(interaction)
 
         # Get the audio from youtube
-        source = YoutubeSource(query)
-        voice.play(source)
-
+        track = YoutubeTrack(query)
+        self.music_manager.process_track(track)
         # Send what will be reproduced
-        await interaction.followup.send(f"Playing {source.title}")
+        await interaction.followup.send(f"Playing {track.title}")
 
 
 async def setup(bot: commands.Bot):
